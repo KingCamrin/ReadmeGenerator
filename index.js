77153589abac66d5de9f7c2bbef4cdd0175cf7c5
Cam <141684224+KingCamrin@users.noreply.github.com>
2024-05-14 17:19:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,86 +1,68 @@
-//Including packages needed for this application
-const fs = require("fs");
-const inquirer = require("inquirer");
-const path = require("path");
-const generateMarkdown = require("./utils/generateMarkdown");
+// Imported required packages
+const fs = require('fs');
+const path = require('path');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
 
-//Creating an array of questions for user input
+// Array of questions for user input
 const questions = [
   {
-    type: "input",
-    name: "title",
-    message: "Please name your Project.",
+    type: 'input',
+    name: 'github',
+    message: 'What is your GitHub username?',
+  },
+  
+  {
+    type: 'input',
+    name: 'title',
+    message: "What is your project's name?",
   },
   {
-    type: "input",
-    name: "description",
-    message: "Please describe the purpose and functionality of this project.",
+    type: 'input',
+    name: 'description',
+    message: 'Please write a short description of your project',
   },
   {
-    type: "input",
-    name: "screenshot",
-    message: "Please provide the relative path to the image you want to use as the screenshot."
+    type: 'list',
+    name: 'license',
+    message: 'What kind of license should your project have?',
+    choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
   },
   {
-    type: "input",
-    name: "link",
-    message: "Please provide a URL where a user can access your deployed application."
+    type: 'input',
+    name: 'installation',
+    message: 'What command should be run to install dependencies?',
+    default: 'npm i',
   },
   {
-    type: "checkbox",
-    name: "license",
-    message: "Please select a license applicable to this project.",
-    choices: ["MIT", "APACHE2.0", "Boost1.0", "MPL2.0", "BSD2", "BSD3", "none"],
+    type: 'input',
+    name: 'test',
+    message: 'What command should be run to run tests?',
+    default: 'npm test',
   },
   {
-    type: "input",
-    name: "require",
-    message: "List any project dependencies here.",
+    type: 'input',
+    name: 'usage',
+    message: 'What does the user need to know about using the repo?',
   },
   {
-    type: "input",
-    name: "features",
-    message: "List some cool features about this project here.",
-  },
-  {
-    type: "input",
-    name: "usage",
-    message:
-      "State the languages or technologies associated with this project.",
-  },
-  {
-    type: "input",
-    name: "creator",
-    message: "Write your GitHub username.",
-  },
-  {
-    type: "input",
-    name: "email",
-    message: "Provide a valid email address.",
-  },
-  {
-    type: "input",
-    name: "contributors",
-    message: "Please list any contributors. (Use GitHub usernames)",
-    default: "",
-  },
-  {
-    type: "input",
-    name: "test",
-    message: "Provide walkthrough of required tests if applicable.",
+    type: 'input',
+    name: 'contributing',
+    message: 'What does the user need to know about contributing to the repo?',
   },
 ];
 
-// Writing README.md File
+// Function to write README file using the user input
 function writeToFile(fileName, data) {
   return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
-// Initializing app
+// Function to initialize app
 function init() {
-  inquirer.prompt(questions).then((responses) => {
-    console.log("Creating Professional README.md File...");
-    writeToFile("./dist/README.md", generateMarkdown({ ...responses }));
+  inquirer.prompt(questions).then((inquirerResponses) => {
+    console.log('Generating README...');
+    writeToFile('README.md', generateMarkdown({ ...inquirerResponses }));
   });
 }
+
 init();
